3129042e4b2937e6d33c27b5a1c243bc216892c7
jcsoares22 <juliocesarwassoares931@gmail.com>
2024-02-07 21:28:21 -0300

    commit

--- shop/lib/components/product_item.dart
+++ shop/lib/components/product_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop/models/cart.dart';
 import 'package:shop/models/product.dart';
 import 'package:shop/utils/app_routes.dart';
 
@@ -7,14 +8,13 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final product = Provider.of<Product>(context, listen: false);
-
+    final cart = Provider.of<Cart>(context, listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         footer: GridTileBar(
           backgroundColor: Colors.black87,
           leading: Consumer<Product>(
-           
             builder: (ctx, product, _) => IconButton(
               onPressed: () {
                 product.toggleFavorite();
@@ -25,11 +25,14 @@ class ProductItem extends StatelessWidget {
             ),
           ),
           title: Text(
-            product.title,
+            product.name,
             textAlign: TextAlign.center,
           ),
           trailing: IconButton(
-            onPressed: () {},
+            onPressed: () {
+              cart.addItem(product);
+              print(cart.itemCount);
+            },
             icon: const Icon(Icons.shopping_cart),
             color: Theme.of(context).colorScheme.secondary,
           ),
@@ -50,4 +53,3 @@ class ProductItem extends StatelessWidget {
     );
   }
 }
-
