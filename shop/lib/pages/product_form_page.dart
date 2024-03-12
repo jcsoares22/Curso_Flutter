884eb02e373ed7c9183e30c7a8cb5607e4f7222e
Júlio César Soares de Almeida <103083326+jcsoares22@users.noreply.github.com>
2024-03-12 12:17:59 -0300

    commit

--- shop/lib/pages/product_form_page.dart
+++ shop/lib/pages/product_form_page.dart
@@ -1,4 +1,7 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ProductFormPage extends StatefulWidget {
   const ProductFormPage({super.key});
@@ -8,13 +11,54 @@ class ProductFormPage extends StatefulWidget {
 }
 
 class _ProductFormPageState extends State<ProductFormPage> {
+  final _priceFocus = FocusNode();
+  final _descriptionFocus = FocusNode();
+
+@override
+ Void dispose(){
+  super.dispose();
+  _priceFocus.dispose();
+ }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: Text('Formulario de produtos'),
-        
-      )
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Form(
+            child: ListView(
+          children: [
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Nome'),
+              textInputAction: TextInputAction.next,
+              onFieldSubmitted: (_){
+                FocusScope.of(context).requestFocus(_priceFocus)
+              },
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'preço'),
+              textInputAction: TextInputAction.next,
+              focusNode: _priceFocus,
+              keyboardType: TextInputType.numberWithOptions(
+                decimal: true,
+              ),
+              onFieldSubmitted: (_){
+                FocusScope.of(context).requestFocus(_descriptionFocus);
+              },
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Descrição'),
+              textInputAction: TextInputAction.next,
+              onFieldSubmitted: (_){
+                FocusScope.of(context).requestFocus(_priceFocus);
+              },
+            ),
+          ],
+        )),
+      ),
     );
   }
 }
