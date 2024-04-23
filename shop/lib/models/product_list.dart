e81d521074801a49cd6211a934b26a9fc7ea5ccc
jcsoares22 <103083326+jcsoares22@users.noreply.github.com>
2024-04-23 12:30:15 -0300

    commit
    Colocando a validação de quando der erro para poder mostrar o status code

--- shop/lib/models/product_list.dart
+++ shop/lib/models/product_list.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shop/exceptions/http_exception.dart';
 
 import 'package:shop/models/product.dart';
 
@@ -91,6 +92,7 @@ class ProductList with ChangeNotifier {
      if (response.statusCode >= 400){
       _items.insert(index, product);
        notifyListeners();
+       throw HttpException(msg: 'Não foi possivel excluir o produto.', statusCode: response.statusCode,);
      }
     }
   }
