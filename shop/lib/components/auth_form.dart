887c3cb73b22ee9d78f8fe5bf900bdc2fd3598d6
jcsoares22 <103083326+jcsoares22@users.noreply.github.com>
2024-05-23 12:45:54 -0300

    commit
    Ajustanto a tela de login

--- shop/lib/components/auth_form.dart
+++ shop/lib/components/auth_form.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+enum AuthMode{ Signup, Login}
+
 class AuthForm extends StatefulWidget {
   const AuthForm({super.key});
 
@@ -8,13 +10,78 @@ class AuthForm extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<AuthForm> {
+  final _passwordController = TextEditingController();
+  AuthMode _authMode = AuthMode.Login;  
+  Map<String, String> _authData = {
+    'Email': '',
+    'password': ''
+  };
+void _submit(){
+
+}
   @override
   Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
     return Card(
+      elevation: 8,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10)
+      ),
       child: Container(
+        padding: const EdgeInsets.all(16),
+        height: 320,
+        width: deviceSize.width * 0.75,
+
         child: Form(child: Column(
           children: [
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'E-mail'),
+              keyboardType: TextInputType.emailAddress,
+              onSaved: (email) => _authData['email'] = email ?? '',
+            ),
+             TextFormField(
+              decoration: const InputDecoration(labelText: 'Senha'),
+              keyboardType: TextInputType.emailAddress,
+              obscureText: true,
+              controller:  _passwordController,
+              onSaved: (password) => _authData['password'] = password ?? '',
+              validator: (_password){ final password = _password ?? '';
+              if(password.isEmpty || password.length <5 ){
+                return 'Informe uma senha válida';
+              }return null; }
+             
+            ),
+            if (_authMode == AuthMode.Signup)
+              TextFormField(
+              decoration: const InputDecoration(labelText: 'Confirmar Senha'),
+              keyboardType: TextInputType.emailAddress,
+              obscureText: true,
+              
+              validator: (_password){
+                final password = _password ?? '';
+                if(password != _passwordController.text){
+                  return 'Senhas informadas n]ao conferem.';
+
+                } return null;
+              },
+            ),
+            SizedBox(
+              height: 20,
+            ), 
+            ElevatedButton(onPressed: _submit, child: Text(
+             _authMode == AuthMode.Login ? 'Entrar' : 'Registrar',
+             
+            ),
+            style:  ElevatedButton.styleFrom(
+              shape:  RoundedRectangleBorder(borderRadius: BorderRadius.circular(30),
+              ), 
+              padding: const EdgeInsets.symmetric(
+                horizontal: 
+                30, vertical: 8,
+              )
+            ),
             
+            )
           ],
         )),
       ),
