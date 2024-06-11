415b4d4372cd5bafb85a17bb2356b6bec7d253c1
jcsoares22 <juliocesarwassoares931@gmail.com>
2024-06-10 21:24:24 -0300

    commit
    Ajustando o banco de dados

--- great/lib/providers/great_places.dart
+++ great/lib/providers/great_places.dart
@@ -3,10 +3,11 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:great/model/place.dart';
+import 'package:great/utils/db_util.dart';
 
 class GreatPlaces with ChangeNotifier {
   final List<Place> _items = [];
-
+  Future<void> lodas
 
   List<Place> get items {
     return [..._items];
@@ -29,6 +30,11 @@ class GreatPlaces with ChangeNotifier {
     );
 
     _items.add(newPlace);
+    DbUtil.insert('places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+    });
     notifyListeners();
   }
 }
