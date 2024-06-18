ff56ed61ff07b5e55842ca07a443bf44dfb82248
jcsoares22 <juliocesarwassoares931@gmail.com>
2024-06-17 21:44:11 -0300

    Commit
    Criando class AuthFormaData

---
+++ chat/lib/model/auth_fform_data.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+enum AuthMode { Signup, Login }
+
+class AuthFormaData {
+  String nome = '';
+  String email = '';
+  String password = '';
+  File? image;
+  AuthMode _mode = AuthMode.Login;
+
+  bool get isLogin {
+    return _mode == AuthMode.Login;
+  }
+
+  bool get isSgnup {
+    return _mode == AuthMode.Signup;
+  }
+
+  void toggleAuthMode() {
+    _mode = isLogin ? AuthMode.Signup : AuthMode.Login;
+  }
+}
