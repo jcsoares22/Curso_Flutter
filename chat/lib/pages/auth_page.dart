9199243d8b6da03375df710fa2c0f7679df76870
jcsoares22 <juliocesarwassoares931@gmail.com>
2024-06-18 21:27:19 -0300

    commit
    commit

--- chat/lib/pages/auth_page.dart
+++ chat/lib/pages/auth_page.dart
@@ -1,17 +1,49 @@
 import 'package:chat/components/auth_form.dart';
+import 'package:chat/model/auth_form_data.dart';
 import 'package:flutter/material.dart';
 
-class AuthPage extends StatelessWidget {
+class AuthPage extends StatefulWidget {
   const AuthPage({super.key});
+
+  @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  bool _isLoading = false;
+
+  void _handleSubmit(AuthFormData formData) {
+    setState(() => _isLoading = true);
+
+    print('AuthPage...');
+    print(formData.email);
+
+    setState(() => _isLoading = false);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 2, 164, 240),
-      body: Center(
-        child: SingleChildScrollView(
-          child: AuthForm(),
-        ),
+      backgroundColor: Colors.blue,
+      body: Stack(
+        children: [
+          Center(
+            child: SingleChildScrollView(
+              child: AuthForm(
+                onSubmit: _handleSubmit,
+              ),
+            ),
+          ),
+          if (_isLoading)
+            Container(
+              decoration: const BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: const Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+        ],
       ),
     );
   }
