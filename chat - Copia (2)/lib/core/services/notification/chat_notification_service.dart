315e6cab49db469bd0cbd0584ba918885c17f090
jcsoares22 <juliocesarwassoares931@gmail.com>
2024-08-02 21:00:03 -0300

    commit
    commit

--- chat - Copia (2)/lib/core/services/notification/chat_notification_service.dart
+++ chat - Copia (2)/lib/core/services/notification/chat_notification_service.dart
@@ -1,15 +1,15 @@
-import 'package:chat/core/model/chat_notification.dart';
+import 'package:chat/core/models/chat_notification.dart';
 import 'package:flutter/material.dart';
 
-class ChatNotificationServices with ChangeNotifier {
+class ChatNotificationService with ChangeNotifier {
   final List<ChatNotification> _items = [];
-
-  List<ChatNotification> get items {
-    return _items;
-  }
 
   int get itemsCount {
     return _items.length;
+  }
+
+  List<ChatNotification> get items {
+    return [..._items];
   }
 
   void add(ChatNotification notification) {
---
+++ chat - Copia (2)/lib/pages/notification_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NotificationPage extends StatelessWidget {
+  const NotificationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Minhas notificações'),
+      ),
+      body: Center(
+        child: Text('notification'),
+      ),
+    );
+  }
+}
